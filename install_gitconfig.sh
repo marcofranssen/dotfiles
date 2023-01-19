6a65ae41e665c34cc522671eca24c7d6145e8345
Marco Franssen <marco.franssen@gmail.com>
2023-01-19 09:45:42 +0100

    Improve install .gitconfig files

--- install_gitconfig.sh
+++ install_gitconfig.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-echo 'Put in place .gitconfig...'
-mkdir -p temp
-curl -sSo temp/.gitconfig https://raw.githubusercontent.com/marcofranssen/dotfiles/main/.gitconfig
+git_config=".gitconfig"
+git_config_private=".gitconfig-private"
 
-function set-git-user() {
+echo 'Put in place .gitconfig...'
+temp="$(mktemp -d)"
+curl -sSo "${temp}/${git_config}" https://raw.githubusercontent.com/marcofranssen/dotfiles/main/${git_config}
+curl -sSo "${temp}/${git_config_private}" https://raw.githubusercontent.com/marcofranssen/dotfiles/main/${git_config_private}
+
+function set-git-user {
   local git_user_name git_user_email
-  git_user_name=$(grep name "$HOME/.gitconfig" | sed 's/^.*= //')
-  git_user_email=$(grep email "$HOME/.gitconfig" | sed 's/^.*= //')
+  git_user_name=$(grep name "$HOME/${1}" | sed 's/^.*= //')
+  git_user_email=$(grep email "$HOME/${1}" | sed 's/^.*= //')
 
   if [ -z "$git_user_name" ]; then
     read -r -p "$(tput setaf 3)Your git username: " git_user_name
@@ -17,28 +21,41 @@ function set-git-user() {
     read -r -p "$(tput setaf 3)Your git user email: " git_user_email
   fi
 
-  git config -f temp/.gitconfig user.name "$git_user_name"
-  git config -f temp/.gitconfig user.email "$git_user_email"
-  sed -i "s/AUTHOR_EMAIL/$git_user_email/" temp/.gitconfig
+  git config -f "${temp}/${1}" user.name "$git_user_name"
+  git config -f "${temp}/${1}" user.email "$git_user_email"
+  sed -i "s/AUTHOR_EMAIL/$git_user_email/" "${temp}/${1}"
 }
 
-set-git-user
+function install-config {
+  if [ -e "${HOME}/${1}" ]; then
+    echo
+    if git --no-pager diff --no-index --minimal "${HOME}/${1}" "${temp}/${1}" ; then
+      echo "${HOME}/${1} is already in sync."
+      exit
+    fi
 
-if [ -e "$HOME/.gitconfig" ]; then
-  echo
-  read -r -p "$(tput setaf 3)Please take a look at the following diff to decide if you want to overwrite your existing .gitconfig$(tput sgr0)"
-  echo
-  git diff ~/.gitconfig temp/.gitconfig
-  echo
-  read -r -p "$(tput setaf 3)Do you want to replace the .gitconfig file at $HOME/.gitconfig?$(tput sgr0) [y/N] " response
-  response=${response,,}
-  if [[ "$response" =~ ^(yes|y)$ ]]; then
-    mv temp/.gitconfig ~/.gitconfig
-    echo Updated your existing .gitconfig
+    echo
+    read -r -p "$(tput setaf 3)Do you want to replace the ${HOME}/${1}?$(tput sgr0) [y/N] " response
+    # remove leading whitespace characters
+    response="${response#"${response%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    response="${response%"${response##*[![:space:]]}"}"
+
+    if [[ "$response" =~ ^(yes|y)$ ]]; then
+      mv "${temp}/${git_config}" "${HOME}/${1}"
+      echo "Updated your existing ${HOME}/${1}".
+    else
+      echo "Your existing ${HOME}/${1} was kept."
+    fi
   else
-    echo Your existing .gitconfig was kept.
+    mv "${temp}/${1}" "${HOME}/${1}"
   fi
-else
-  mv temp/.gitconfig ~/.gitconfig
-fi
-rm -r temp
+}
+
+set-git-user ${git_config}
+set-git-user ${git_config_private}
+
+install-config ${git_config}
+install-config ${git_config_private}
+
+rm -r "${temp}"
