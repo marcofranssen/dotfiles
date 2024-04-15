5e2c9ec5cefadf94c83b0bbba2ab6c1d39429138
Marco Franssen <marco.franssen@gmail.com>
2024-04-15 12:33:52 +0200

    Add gh completion to oh-my-zsh config

---
+++ .oh-my-zsh/custom/plugins/gh/gh.plugin.zsh
@@ -0,0 +1,7 @@
+# Add your own custom plugins in the custom/plugins directory. Plugins placed
+# here will override ones with the same name in the main plugins directory.
+# See: https://github.com/ohmyzsh/ohmyzsh/wiki/Customization#overriding-and-adding-plugins
+
+if hash gh 2>/dev/null; then
+  source <(gh completion -s zsh)
+fi
