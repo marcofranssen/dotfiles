2fc8b09cd0af3171cb6ba93e457e1adf3de9ea70
Marco Franssen <marco.franssen@gmail.com>
2026-04-23 12:16:03 +0200

    Switch t alias from terraform to opentofu

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -2,6 +2,6 @@ alias reload="omz reload"
 alias zshconfig="vi ~/.zshrc"
 alias ohmyzsh="vi ~/.oh-my-zsh"
 alias sed='gsed'
-alias t=terraform
+alias t=tofu
 # alias docker=nerdctl
 # alias gws="PATH=/usr/local/opt/coreutils/libexec/gnubin:usr/local/opt/gnu-sed/libexec/gnubin:$PATH gws"
