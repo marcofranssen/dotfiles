c768799316b95d417bc569fb7d90a8448dfc7fd7
Marco Franssen <marco.franssen@gmail.com>
2026-04-28 23:15:33 +0200

    neovim: Add helm-ls and kube-linter

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -1,3 +1,19 @@
+-- Detect helm templates: yaml files inside a templates/ dir with a Chart.yaml ancestor
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*/templates/*.yaml",
+  group = vim.api.nvim_create_augroup("helm-filetype", { clear = true }),
+  callback = function(event)
+    local dir = vim.fn.fnamemodify(event.file, ":h")
+    while dir ~= "/" do
+      dir = vim.fn.fnamemodify(dir, ":h")
+      if vim.fn.filereadable(dir .. "/Chart.yaml") == 1 then
+        vim.bo[event.buf].filetype = "helm"
+        return
+      end
+    end
+  end,
+})
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
