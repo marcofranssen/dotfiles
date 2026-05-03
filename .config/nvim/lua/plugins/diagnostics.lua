896c155896d60e43e295b93f082aa246b98ff077
Marco Franssen <marco.franssen@gmail.com>
2026-05-03 17:35:13 +0200

    neovim: Add diagnostics

---
+++ .config/nvim/lua/plugins/diagnostics.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufWritePost", "BufReadPost" },
+    config = function()
+      local lint = require("lint")
+      lint.linters_by_ft = {
+        helm = { "kube_linter" },
+      }
+      vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
+        group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+    },
+  },
+}
