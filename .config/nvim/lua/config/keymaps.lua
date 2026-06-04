723611531a788c01441755573abab23026e1b008
Marco Franssen <marco.franssen@gmail.com>
2026-06-04 09:25:11 +0200

    nvim: Clear search on ESC

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,7 @@ end
 map("x", "p", '"_dP', { noremap = true, silent = true })
 map({ "n", "x" }, "d", '"_d', { noremap = true, silent = true })
 map({ "n", "x" }, "c", '"_c', { noremap = true, silent = true })
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+map("n", "<Esc>", "<cmd>nohlsearch<CR>")
