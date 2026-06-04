3720dec98fdafaa59ed1ee4018e7c7e6c46f1fb3
Marco Franssen <marco.franssen@gmail.com>
2026-06-04 09:21:48 +0200

    nvim: Prevent yank on delete, change, paste

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,7 @@ local function map(mode, key, cmd, opts)
   vim.keymap.set(mode, key, cmd, opts)
 end
 
+-- Change delete, change, and paste to not yank text
+map("x", "p", '"_dP', { noremap = true, silent = true })
+map({ "n", "x" }, "d", '"_d', { noremap = true, silent = true })
+map({ "n", "x" }, "c", '"_c', { noremap = true, silent = true })
