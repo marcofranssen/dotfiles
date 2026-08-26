83741f065addc0594e0d452ae88239707a2dabac
Marco Franssen <marco.franssen@gmail.com>
2026-08-26 09:21:10 +0200

    nvim: Add keybindings to move windows

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,3 +11,9 @@ map({ "n", "x" }, "c", '"_c', { noremap = true, silent = true })
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
+-- map("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
+-- map("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
+-- map("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
+-- map("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
